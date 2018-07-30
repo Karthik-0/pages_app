016d4c1385e66048c5ef53d6e1564c0ddb4753ec
karthikraja34 <rajakarthik131@gmail.com>
2018-07-30 12:13:22 +0530

    Remove trailing whitespace

--- pagesapp/urls.py
+++ pagesapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PageIndex.as_view(), name="index"),
-    path('add/', views.PageAdd.as_view(), name="add"),    
+    path('add/', views.PageAdd.as_view(), name="add"),
     path('<slug>/', views.PageDetail.as_view(), name="detail"),
 ]
